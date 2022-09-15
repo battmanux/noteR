9ecfca4f20487ddddbec92282754646548500b37
Emmanuel BATT <emmanuel.cran@battfamily.fr>
2022-09-15 21:11:52 +0000

    fix query while loading

--- inst/gadget.R
+++ inst/gadget.R
@@ -7,14 +7,131 @@ ui <- fluidPage(
   htmlOutput(outputId = "content")
 )
 
+searchString <- function(input, output, gState) {
+  {
+    if ( input$search == "" && gState$documentType == "qmd" ) {
+      output$content <- renderUI(docParameters(gState$documentPath))
+      return()
+    }
+
+    rstudioapi::documentSaveAll()
+
+    if (!is.null(input$search) && nchar(input$search) > 2) {
+      suppressWarnings({
+        l_res <- system(command = paste0("grep -R -i -I -n -H '", input$search, "' ",gState$projectFolder,"/* ") ,intern = T, ignore.stderr = T, timeout = 100)
+      })
+
+      if ( length(l_res) == 0 )
+        return()
+
+      l_list <- strsplit(l_res, ":")
+      l_data <- data.table(
+        path= unlist(lapply(l_list, function(x) x[[1]])),
+        line= unlist(lapply(l_list, function(x) x[[2]])),
+        match= unlist(lapply(l_list, function(x) paste(collapse = ":", x[3:length(x)]) ) )
+      )
+
+      l_data$file_type <- gsub(pattern = "^.*\\.([a-zA-Z]+)$", replacement = "\\1", x =l_data$path )
+
+
+
+      l_data[,title:=getTitleFromQmdv(path)]
+      l_data[,filename:= gsub(pattern = "^(.*)\\.[a-zA-Z]+$", replacement = "\\1", x = basename(path) )]
+
+      l_data <- l_data[,.SD[1,], by=.(path)]
+
+      l_data[,order:=0]
+      l_data[,order:=order+(stringdist::stringdist(title, input$search)/nchar(title)) ]
+      l_data[file_type=="qmd",order:=order-5]
+
+
+      setkeyv(l_data, "order")
+
+      output$content <- renderUI({
+
+        if (nrow(l_data) > 0) {
+          l_found <- tags$button(type="button", class="list-group-item",
+                                 lapply(seq_len(nrow(l_data)), function(i) {
+                                   x <-  l_data[i,]
+                                   l_elem <- tags$a(class="list-group-item", target=x[,path],
+                                                    onClick=paste0("Shiny.setInputValue('openfile', '",x[,path],"', {priority : 'event'})"),
+                                                    tags$h4(x[,title], ' ', class="list-group-item-heading"),
+                                                    tags$small(tags$p(x[,path])),
+                                                    tags$p(x[,match], class="list-group-item-text" )
+                                   )
+                                   return(l_elem)
+                                 } )
+          )
+        } else {
+          l_found <- list()
+        }
+        l_create <- tags$button(type="button", class="list-group-item",
+                                tags$a(class="list-group-item", target=input$search,
+                                       tags$h4(paste0("Create '", input$search, "'"),  class="list-group-item-heading"),
+                                       tags$p(paste("Use default template"), class="list-group-item-text" )
+                                )
+        )
+        tags$small(l_found, l_create)
+      } )
+    }
+  }
+}
+
+getTitleFromQmd <-function(x) {
+  l_content <- readLines(x)
+  l_first_match <- grep(pattern = "^title: .*$", value = T, x = l_content)
+
+  if (length(l_first_match) == 0)
+    return( gsub(pattern = "^(.*)\\.[a-zA-Z]+$", replacement = "\\1", x = basename(x) ))
+
+  l_title <- gsub("title: *", "", l_first_match[[1]])
+
+  return(l_title)
+}
+
+getParametersFromQmd <-function(x) {
+  l_content <- readLines(x)
+  l_first_match <- which(grepl(pattern = "^(\`\`\`.*yaml)|(\\-\\-\\-)", x = l_content))
+  l_last_match <- which(grepl(pattern = "^(\`\`\`)|(\\-\\-\\-)", x = l_content))
+
+  l_out <- list()
+  l_state <- 'out'
+  for ( i in l_first_match) {
+    if ( l_content[i] == "---" && l_state == 'out') {
+      l_state <- "in"
+    } else if ( l_content[i] == "---" && l_state == 'in') {
+      l_state <- "out"
+      next
+    }
+
+    e <- l_last_match[l_last_match>i][[1]]
+    l_yaml <- yaml::yaml.load(
+      paste(collapse = "\n", l_content[(i+1):(e-1) ])
+    )
+
+    if ( l_state == 'in' ) {
+      l_out[[i]] <- list(qmd=l_yaml)
+    } else {
+      l_out[[i]] <- l_yaml
+    }
+  }
+
+  l_ret <- do.call(c, l_out)
+  return(l_ret)
+}
+
+getTitleFromQmdv <- Vectorize(getTitleFromQmd)
+
 server <- function(input, output, session) {
 
   gProjectFolder <- rstudioapi::getActiveProject()
   if ( gProjectFolder == "" ) {
     gProjectFolder <- getwd()
   }
-  
+  cat("Folder: ", gProjectFolder, "\n")
+
   gState <- reactiveValues(
+    projectFolder = gProjectFolder,
     documentId = "",
     documentType = "",
     documentPath = "",
@@ -24,12 +141,12 @@ server <- function(input, output, session) {
     ctx = 0
   )
 
-  gState_ctx <- list()
+  gState_ctx <- list(id="")
 
-  observe({
+  observeEvent(input$show_prm, {
     try({
       gState_ctx <<- rstudioapi::getSourceEditorContext()
-      invalidateLater(500)
+      #invalidateLater(500)
       if ( gState$documentId != gState_ctx$id ||
            gState$selection !=  rstudioapi::selectionGet(id=gState_ctx$id)$value) {
           gState$ctx <- gState$ctx + 1
@@ -37,12 +154,9 @@ server <- function(input, output, session) {
     })
   })
 
-  observeEvent(gState$ctx, {
-    # str(gState)
-    if ( nchar(gState$gotofile) > 0 && gState$gotofile != gState$documentPath ) {
-      rstudioapi::navigateToFile(gState$gotofile)
-      return()
-    }
+
+  observeEvent(gState$ctx,{
+
     l_ctx <- gState_ctx
     if ( gState$documentId != l_ctx$id ) {
       gState$documentId <- l_ctx$id
@@ -70,11 +184,7 @@ server <- function(input, output, session) {
     }
   })
 
-  observeEvent(gState$selection, {
-    updateTextInput(inputId = "search", value = gState$selection )
-  })
-
-  observeEvent(gState$documentId, {
+  observeEvent(gState$documentPath, {
     ## Parse doc
     if (gState$documentType == "qmd" ) {
       output$content <- renderUI(docParameters(gState$documentPath))
@@ -84,12 +194,22 @@ server <- function(input, output, session) {
   })
 
   docParameters <- function(path) {
+    l_params <- getParametersFromQmd(path)
+    l_qmd <- l_params$qmd
+    l_params$qmd <- NULL
+    if (!is.null(l_params$logo)) {
+      l_logo <- tags$img(src=l_params$logo, style='max-height:30px;margin-left: 10px;')
+    } else {
+      l_logo <- icon("file", style='max-height:30px;margin-left: 10px;')
+    }
     return(
       div(
-        tags$h2("other doc"),
-        tags$ul(
-          tags$li("some doc")
-        ),
+        tags$h2(l_logo , l_qmd$title),
+        tags$table(class="table",
+          lapply(names(l_params), function(n) {
+            tags$tr(tags$td(n), tags$td( l_params[[n]] ))
+          }
+        ) ),
         shiny::HTML('
         <style>
             #drop_zone {
@@ -138,86 +258,24 @@ function dragOverHandler(ev) {
   }
 
   observeEvent(input$search, {
-    if ( input$search == "" && gState$documentType == "qmd" ) {
-      output$content <- renderUI(docParameters(gState$documentPath))
-    }
-
-    rstudioapi::documentSaveAll()
-
-    if (!is.null(input$search) && nchar(input$search) > 2) {
-      suppressWarnings({
-        l_res <- system(command = paste0("grep -R -i -I -n -H '", input$search, "' ",gProjectFolder," ") ,intern = T, ignore.stderr = T, timeout = 100)
-      })
-
-      l_list <- strsplit(l_res, ":")
-      l_data <- data.table(
-        path= unlist(lapply(l_list, function(x) x[[1]])),
-        line= unlist(lapply(l_list, function(x) x[[2]])),
-        match= unlist(lapply(l_list, function(x) paste(collapse = ":", x[3:length(x)]) ) )
-        )
-      
-      l_data$file_type <- gsub(pattern = "^.*\\.([a-zA-Z]+)$", replacement = "\\1", x =l_data$path )
-
-      getTitleFromQmd <-function(x) {
-        l_content <- readLines(x)
-        l_first_match <- grep(pattern = "^title: .*$", value = T, x = l_content)
-        
-        if (length(l_first_match) == 0)
-          return( gsub(pattern = "^(.*)\\.[a-zA-Z]+$", replacement = "\\1", x = basename(x) ))
-        
-        l_title <- gsub("title: *", "", l_first_match[[1]])
-        
-        return(l_title)
-      }
-      
-      getTitleFromQmdv <- Vectorize(getTitleFromQmd)
-      
-      l_data[,title:=getTitleFromQmdv(path)]
-      l_data[,filename:= gsub(pattern = "^(.*)\\.[a-zA-Z]+$", replacement = "\\1", x = basename(path) )]
-      
-      l_data[,order:=0]
-      l_data[,order:=order+(stringdist::stringdist(title, input$search)/nchar(title)) ]
-      l_data[file_type=="qmd",order:=order-5]
-      
-      setkeyv(l_data, "order")
-      
-      output$content <- renderUI({
-
-        if (nrow(l_data) > 0) {
-          l_found <- tags$button(type="button", class="list-group-item",
-                                 lapply(seq_len(nrow(l_data)), function(i) {
-                                  x <-  l_data[i,]
-                                   l_elem <- tags$a(class="list-group-item", target=x[,path], 
-                                                    onClick=paste0("Shiny.setInputValue('openfile', '",x[,path],"')"), 
-                                                    tags$h4(x[,title], ' ', class="list-group-item-heading"),
-                                                    tags$small(tags$p(x[,path])),
-                                                    tags$p(x[,match], class="list-group-item-text" )
-                                   )
-                                   return(l_elem)
-                                 } )
-          )
-        } else {
-          l_found <- list()
-        }
-        l_create <- tags$button(type="button", class="list-group-item",
-                      tags$a(class="list-group-item", target=input$search,
-                             tags$h4(paste0("Create '", input$search, "'"),  class="list-group-item-heading"),
-                             tags$p(paste("Use default template"), class="list-group-item-text" )
-                      )
-          )
-        tags$small(l_found, l_create)
-      } )
-    }
+    searchString(input, output, gState)
   } )
 
-  ## TODO: reactive value : document id
-  ## on link creation, if visual, inset {create link to} + save + replace + reload
-  # rstudioapi::selectionSet("fdf", id = "25EDE74F")
+  observeEvent(input$import_search, {
+    if (input$search == "") {
+      updateTextInput(inputId = "search", value = rstudioapi::selectionGet()$value)
+    } else {
+      searchString(input, output, gState)
+    }
+  })
 
   observeEvent(input$openfile, {
-    gState$gotofile <- input$openfile
+    if ( file.exists(input$openfile)) {
+      rstudioapi::navigateToFile(input$openfile)
+      gState$documentPath <- input$openfile
+    }
   })
-    
+
   observeEvent(input$new, {
     l_new_file <-  paste0(input$new_file_name, ".qmd")
     l_content <- readLines("~/saved_data/templates/default.qmd", warn = F)
